635a08b6bef1d5748f3e626230e0b8c506cd587d
Thauler <thauler98@gmail.com>
2022-10-10 20:24:51 -0300

    feat(models): implements the entity User
    This entity has three columns 'id', 'name' and 'email' and his getters and setters.

---
+++ models/User.php
@@ -0,0 +1,60 @@
+<?php
+
+class User
+{
+    private int $id;
+    private string $name;
+    private string $email;
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return void
+     */
+    public function setId(int $id): void
+    {
+        $this->id = trim($id);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return void
+     */
+    public function setName(string $name): void
+    {
+        $this->name = ucwords(trim($name));
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return void
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = strtolower(trim($email));
+    }
+}
